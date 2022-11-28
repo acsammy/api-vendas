9f94efd4e1bff8a1efc19c144d9e9020f47a61cf
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-11-28 16:16:59 -0200

    fixing refactoring

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import { celebrate, Segments, Joi } from 'celebrate';
 import UsersController from '../controllers/UsersController';
-import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
+import isAuthenticated from '@shared/infra/http/middlewares/isAuthenticated';
 import UserAvatarController from '../controllers/UserAvatarController';
 import multer from 'multer';
 import uploadConfig from '@config/upload';

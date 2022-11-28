6dc01590483ac8ad710b6a7571e67ba2214dfaeb
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-11-28 11:33:02 -0200

    refactoring redis cache

--- src/modules/products/services/ListProductService.ts
+++ src/modules/products/services/ListProductService.ts
@@ -1,4 +1,4 @@
-import RedisCache from '@shared/cache/RedisCache';
+import redisCache from '@shared/cache/RedisCache';
 import { getCustomRepository } from 'typeorm';
 import Product from '../typeorm/entities/Product';
 import { ProductRepository } from '../typeorm/repositories/ProductsRepository';
@@ -7,7 +7,7 @@ export default class ListProductService {
   public async execute(): Promise<Product[]> {
     const productRepository = getCustomRepository(ProductRepository);
 
-    const redisCache = new RedisCache();
+    //const redisCache = new RedisCache();
 
     let products = await redisCache.recover<Product[]>(
       'api-vendas-PRODUCT_LIST',

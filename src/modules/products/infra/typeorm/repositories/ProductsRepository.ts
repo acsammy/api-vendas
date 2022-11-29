3184850be4b8fa73ee36f51950049248ca296e5c
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-11-29 16:43:37 -0200

    refactoring adjustment

--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -1,6 +1,7 @@
 import { ICreateProduct } from '@modules/products/domain/models/ICreateProduct';
 import { IFindProducts } from '@modules/products/domain/models/IFindProducts';
 import { IProduct } from '@modules/products/domain/models/IProduct';
+import { IUpdateStockProduct } from '@modules/products/domain/models/IUpdateStockProduct';
 import { IProductsRepository } from '@modules/products/domain/repositories/IProductsRepository';
 import { getRepository, In, Repository } from 'typeorm';
 import Product from '../entities/Product';
@@ -10,6 +11,22 @@ export class ProductRepository implements IProductsRepository {
 
   constructor() {
     this.ormRepository = getRepository(Product);
+  }
+
+  public async findAll(): Promise<IProduct[]> {
+    const product = await this.ormRepository.find();
+    return product;
+  }
+  public async findByName(name: string): Promise<IProduct | undefined> {
+    const product = this.ormRepository.findOne({ where: { name } });
+    return product;
+  }
+  public async findById(id: string): Promise<IProduct | undefined> {
+    const product = this.ormRepository.findOne(id);
+    return product;
+  }
+  public async updateStock(products: IUpdateStockProduct[]): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 
   public async create({
@@ -33,21 +50,6 @@ export class ProductRepository implements IProductsRepository {
 
   public async remove(product: Product): Promise<void> {
     await this.ormRepository.remove(product);
-  }
-
-  public async findbyName(name: string): Promise<IProduct | undefined> {
-    const product = this.ormRepository.findOne({ where: { name } });
-    return product;
-  }
-
-  public async findAll(): Promise<IProduct[] | undefined> {
-    const products = await this.ormRepository.find();
-    return products;
-  }
-
-  public async findbyId(id: string): Promise<Product | undefined> {
-    const product = this.ormRepository.findOne(id);
-    return product;
   }
 
   public async findAllByIds(products: IFindProducts[]): Promise<IProduct[]> {
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,36 @@
-import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepositorys';
-import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
 import { container } from 'tsyringe';
+import { ICustomersRepository } from '@modules/customers/domain/repositories/ICustomersRepositorys';
+import { IProductsRepository } from '@modules/products/domain/repositories/IProductsRepository';
+import { IOrdersRepository } from '@modules/orders/domain/repositories/IOrderRepository';
+import { IUsersRepository } from '@modules/users/domain/repositories/IUsersRepository';
+import { IUserTokensRepository } from '@modules/users/domain/repositories/IUserTokensRepository';
+import CustomersRepository from '@modules/customers/infra/typeorm/repositories/CustomersRepository';
+import { ProductRepository } from '@modules/products/infra/typeorm/repositories/ProductsRepository';
+import { OrdersRepository } from '@modules/orders/infra/typeorm/repositories/OrdersRepository';
+import { UsersRepository } from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import { UserTokensRepository } from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<ICustomersRepository>(
   'CustomersRepository',
   CustomersRepository,
+);
+
+container.registerSingleton<IProductsRepository>(
+  'ProductsRepository',
+  ProductRepository,
+);
+
+container.registerSingleton<IOrdersRepository>(
+  'OrdersRepository',
+  OrdersRepository,
+);
+
+container.registerSingleton<IUsersRepository>(
+  'UsersRepository',
+  UsersRepository,
+);
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository,
 );

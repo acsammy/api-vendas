ac6414e197acf88651926432bc1eb63380574e2f
Samuel Carvalho <carvalho.asamuel@gmail.com>
2022-11-30 08:57:19 -0200

    refactoring tests

--- src/modules/customers/services/__tests/CreateCustomerService.spec.ts
+++ src/modules/customers/services/__tests/CreateCustomerService.spec.ts
@@ -2,12 +2,18 @@ import 'reflect-metadata';
 import FakeCustomersRepository from '@modules/customers/domain/repositories/Fakes/FakeCustomersRepository';
 import '../CreateCustomerService';
 import CreateCustomerService from '../CreateCustomerService';
+import AppError from '@shared/errors/AppError';
+
+let fakeCustomerRepository: FakeCustomersRepository;
+let createCustomer: CreateCustomerService;
 
 describe('CreateCustomer', () => {
-  it('should be able to create a new customer.', async () => {
-    const fakeCustomerRepository = new FakeCustomersRepository();
-    const createCustomer = new CreateCustomerService(fakeCustomerRepository);
+  beforeEach(() => {
+    fakeCustomerRepository = new FakeCustomersRepository();
+    createCustomer = new CreateCustomerService(fakeCustomerRepository);
+  });
 
+  it('should be able to create a new customer.', async () => {
     const customer = await createCustomer.execute({
       name: 'Samuel Carvalho',
       email: 'samuel@samuel.com',
@@ -17,14 +23,16 @@ describe('CreateCustomer', () => {
   });
 
   it('should not be able to create two customer with same email.', async () => {
-    const fakeCustomerRepository = new FakeCustomersRepository();
-    const createCustomer = new CreateCustomerService(fakeCustomerRepository);
-
-    const customer = await createCustomer.execute({
+    await createCustomer.execute({
       name: 'Samuel Carvalho',
       email: 'samuel@samuel.com',
     });
 
-    expect(customer).toHaveProperty('id');
+    expect(
+      createCustomer.execute({
+        name: 'Samuel Alves',
+        email: 'samuel@samuel.com',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
